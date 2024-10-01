1caa883dbf9ed23220d6648218f0191a3ee000fc
hevilyn21 <hevilyn.abreu.oliveira@escola.pr.gov.br>
2024-10-01 07:53:18 -0300

    Add files via upload

---
+++ Página web desenvolvendo uma ferramenta interativa de estudo/perguntas.js
@@ -0,0 +1,60 @@
+criaCartao( //cria o cartao 
+    'Internet', //tópico da pergunta
+    'Quando a internet foi criada?', //pergunta
+    'ddd' //resposta
+)
+
+criaCartao(//cria o cartao 
+    'Inteligêngia Artificial', //tópico da pergunta
+    'O que é Inteligência Artificial?', //pergunta
+    'A capital da França é Paris' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Internet', //tópico da pergunta
+    'O que é uma função?', //pergunta
+    'Uma função é um bloco de código que executa alguma tarefa' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Inteligêngia Artificial', //tópico da pergunta
+    'Como se diz oi em Inglês?', //pergunta
+    'Oi em ingles é HI (RAI)' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Inteligêngia Artificial', //tópico da pergunta
+    'O que é Python?', //pergunta
+    'O Python é uma linguagem de programação' //resposta
+)
+
+criaCartao(//cria o cartao 
+    'Internet', //tópico da pergunta
+    'Qual a capital da França?', //pergunta
+    'A capital da França é Paris' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'LGPD', //tópico da pergunta
+    'O que é uma função?', //pergunta
+    'Uma função é um bloco de código que executa alguma tarefa' //resposta
+)
+
+
+criaCartao( //cria o cartao 
+    'LGPD', //tópico da pergunta
+    'Como se diz oi em Inglês?', //pergunta
+    'Oi em ingles é HI (RAI)' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'LGPD', //tópico da pergunta
+    'Como se diz oi em Inglês?', //pergunta
+    'Oi em ingles é HI (RAI)' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'LGPD', //tópico da pergunta
+    'Como se diz oi em Inglês?', //pergunta
+    'Oi em ingles é HI (RAI)' //resposta
+)
